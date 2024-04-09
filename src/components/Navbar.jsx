ab56930327757fd85e1f072cb764cd3065d1aa8e
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-04-10 00:19:06 +0100

    Create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,66 @@
+import { useState, useEffect } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { disablePageScroll, enablePageScroll } from 'scroll-lock';
 
+import { navigation } from '../data';
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	// const pathname = useLocation();
 
-export default Navbar
+	// const [menuMobile, setMenuMobile] = useState(false);
+
+	// const [menuBtn, setMenuBtn] = useState(false);
+
+	// const toggleMenu = () => {
+	// 	setMenuMobile((active) => !active);
+	// 	setMenuBtn((active) => !active);
+
+	// 	const section = document.getElementById(id);
+	// 	if (section) {
+	// 		section.scrollIntoView({ behavior: 'smooth' });
+	// 	}
+	// };
+
+	const [scrollPosition, setScrollPosition] = useState(0);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			setScrollPosition(window.scrollY);
+		};
+
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	return (
+		<div className={`navbar ${scrollPosition > 60 ? 'navbar__scrolled' : ''}`}>
+			<div className="container">
+				<div className="navbar__wrapper">
+					<div className="logo">
+						
+					</div>
+					<nav>
+						<ul className="navbar__menu">
+							{navigation.map((item) => {
+								<li>
+									<NavLink key={item.id} to={item.url}>
+										{item.title}
+									</NavLink>
+								</li>;
+							})}
+						</ul>
+					</nav>
+					{/* <div className={menuBtn ? 'navbar__hamburger active' : 'navbar__hamburger'} onClick={toggleMenu}>
+						<span className="bar"></span>
+						<span className="bar"></span>
+						<span className="bar"></span>
+					</div> */}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 function Home() {
 	return (
 		<div>
-			<h6>Get into</h6>
-			<h1>Fitness</h1>
-			{/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolore?</p> */}
+		
 		</div>
 	);
 }
