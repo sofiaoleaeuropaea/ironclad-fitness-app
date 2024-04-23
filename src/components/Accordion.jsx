5130a2f6164d84e19abbb5f2b8157f2cd74e9700
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-04-23 19:41:25 +0100

    Solve accordion bugs

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,10 +1,22 @@
+import { useRef} from 'react';
+const Accordion = ({ id, handleToggle, index, accordionOpen, title, content }) => {
+	const contentEl = useRef(null);
 
-const Accordion =() => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<div key={id} className="accordion">
+			<button className="accordion__heading" onClick={() => handleToggle(index)}>
+				<span className={`accordion__heading__title ${accordionOpen === index ? 'active' : ''}`}>{title}</span>
+				<div className={`accordion__heading__arrow ${accordionOpen === index ? 'active' : ''}`}>
+					<span className="bar"></span>
+					<span className="bar"></span>
+				</div>
+			</button>
 
-export default Accordion
+			<div ref={contentEl} className={`accordion__content ${accordionOpen === index ? 'active' : ''}`} style={accordionOpen === index ? { height: contentEl.current.scrollHeight } : { height: '0px' }}>
+				<ul>{content}</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Accordion;
--- src/pages/about/Partners.jsx
+++ src/pages/about/Partners.jsx
@@ -1,17 +1,31 @@
 import { NavLink } from 'react-router-dom';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
-import { IoIosArrowForward, IoIosArrowDown } from 'react-icons/io';
-
-import Heading from '../../components/Heading';
 import { partners } from '../../data';
 
+import Heading from '../../components/Heading';
+import Accordion from '../../components/Accordion';
+
+
+
 const Partnership = () => {
-	const contentEl = useRef(null)
+
 	const [accordionOpen, setAccordionOpen] = useState(null);
 	const handleToggle= (index) => {
-		setAccordionOpen(index === accordionOpen ? null : index);
+		setAccordionOpen((prevIndex) => (prevIndex === index ? null : index));
 	};
+
+	// const brandList = () => {
+	// 	return 
+	// 		item.brands.map((brand, index) => (
+	// 			<li key={index}>
+	// 			<NavLink to={brand.url}>
+	// 			<img src={brand.logo} alt={brand.name} className="img-fluid partner__img" />
+	// 			</NavLink>
+	// 			</li>
+	// 		))
+	
+	// }
 
 	return (
 		<section id="partner" className="partner">
@@ -20,29 +34,22 @@ const Partnership = () => {
 				paragraph="At our fitness center, partnerships serve as the cornerstone of our dedication to cultivating a vibrant fitness community. We prioritize collaboration with top-tier brands and fitness authorities to elevate our members' gym experience. Through these partnerships, we provide exclusive perks, access to state-of-the-art equipment and training regimens, and thrilling events designed to ignite inspiration and motivation."
 			/>
 			<div className="container">
-				<div className="partner__wrapper">
-					{partners.map((item, index) => (
-						<div key={item.id} className="partner__accordion">
-							<button className="partner__accordion__title" onClick={() => handleToggle(index)}>
-								<span>{item.title}</span>
-								<div className={` arrow ${accordionOpen === index ? 'active' : ''}`}>
-									<span className="bar"></span>
-									<span className="bar"></span>
-								</div>
-							</button>
-
-							<div ref={contentEl} className={`partner__accordion__content ${accordionOpen === index ? 'active' : ''}`} style={accordionOpen === index ? {height: contentEl.current.scrollHeight} : {height: "0px"}}>
-								<ul>
-									{item.brands.map((brands, index) => (
-										<li key={index}>
-											<NavLink to={brands.url}>
-												<img src={brands.logo} alt={brands.name} className="img-fluid partner__img" />
-											</NavLink>
-										</li>
-									))}
-								</ul>
-							</div>
-						</div>
+				<div className="partner__accordion">
+					{partners.map((partner, index) => (
+						<Accordion
+							id={partner.id}
+							title={partner.title}
+							content={partner.brands.map((brand, index) => (
+								<li key={index}>
+									<NavLink to={brand.url}>
+										<img src={brand.logo} alt={brand.name} className="img-fluid partner__img" />
+									</NavLink>
+								</li>
+							))}
+							index={index}
+							accordionOpen={accordionOpen}
+							handleToggle={handleToggle}
+						/>	
 					))}
 				</div>
 			</div>
