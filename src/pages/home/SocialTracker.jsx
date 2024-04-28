f72f42da478f15d9bd50163276b529da59040ccb
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-04-28 13:05:31 +0100

    Correct bug on hero slider

--- src/pages/home/SocialTracker.jsx
+++ src/pages/home/SocialTracker.jsx
@@ -1,23 +1,31 @@
 import { socialTracker } from '../../data';
 
 function SocialTracker() {
-	const SocialTrackerList = () => {
-		return socialTracker.map((item) => {
-			const [categoryWordOne, categoryWordTwo] = item.category.split(' ');
+	const duplicatedSlides = [...socialTracker, ...socialTracker];
+	const SocialTrackerSlider = () => {
+		return duplicatedSlides.map((slide, index) => {
+			// const [categoryWordOne, categoryWordTwo] = item.category.split(' ');
 			return (
-				<div key={item.id} className="social-tracker__item" style={{ display: 'flex' }}>
-					<span>{item.tracker} </span>
+				
+					<div key={index} className="social-tracker__list" style={{ width: `${100 / slide.length}%` }}>
+						<div className="social-tracker__item">
+							<p>
+								{slide.tracker} <span> {slide.category} </span>{' '}
+							</p>
+						</div>
+						{/* <span>{item.tracker} </span>
 					<p>{categoryWordOne}</p>
-					<p>{categoryWordTwo}</p>
-				</div>
+					<p>{categoryWordTwo}</p> */}
+					</div>
+			
 			);
 		});
 	};
 
 	return (
 		<section id="social-tracker" className="social-tracker">
-			<div className="social-tracker__list">
-				<SocialTrackerList />
+			<div className="social-tracker__slider">
+				<SocialTrackerSlider />
 			</div>
 		</section>
 	);
