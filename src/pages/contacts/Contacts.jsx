a4fb6b4bca6273ee7f09745ccecaeab4ed70beb0
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-05-07 22:16:51 +0100

    Add mapbpx, add styles to it

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,34 +1,38 @@
 import FormContact from './FormContact';
+import DirectionsMap from './DirectionsMap';
 const Contacts = () => {
 	return (
 		<>
 			<section id="contact__information" className="contact__information">
 				<div className="container ">
 					<div className="contact__wrapper ">
-						<h2>How to find us</h2>
-						<div className="contact__details">
-							<div className="contact__details_schedule">
-								<p>
-									<span> Mon-Fri</span> <time dateTime="T06:00">6:00 am</time> - <time dateTime="T22:30">10:30 pm</time>
-								</p>
+						<div className="contact__information__wrapper ">
+							<div className="contact__information__details">
+								<h2>How to find us</h2>
+								<div className="contact__details_schedule">
+									<p>
+										<span> Mon-Fri</span> <time dateTime="T06:00">6:00 am</time> - <time dateTime="T22:30">10:30 pm</time>
+									</p>
 
-								<p>
-									<span> Sat</span> <time dateTime="T09:00">9:00 am</time> - <time dateTime="T20:00">8:00 pm</time>
-								</p>
+									<p>
+										<span> Sat</span> <time dateTime="T09:00">9:00 am</time> - <time dateTime="T20:00">8:00 pm</time>
+									</p>
 
-								<p>
-									<span> Sun</span> <time dateTime="T10:00">10:00 am</time> - <time dateTime="T18:00">6:00 pm</time>
-								</p>
+									<p>
+										<span> Sun</span> <time dateTime="T10:00">10:00 am</time> - <time dateTime="T18:00">6:00 pm</time>
+									</p>
+								</div>
+								<address>
+									<a>Edifício Mirage, R. Dr. Eduardo Neves, Nº3 1050-077 Lisboa</a>
+								</address>
+								<address>
+									<a>querosabermais@flag.pt</a>
+								</address>
+								<address>
+									<a>(+351) 239000000</a>
+								</address>
 							</div>
-							<address>
-								<a>Edifício Mirage, R. Dr. Eduardo Neves, Nº3 1050-077 Lisboa</a>
-							</address>
-							<address>
-								<a>querosabermais@flag.pt</a>
-							</address>
-							<address>
-								<a>(+351) 239000000</a>
-							</address>
+							<DirectionsMap />
 						</div>
 					</div>
 				</div>
@@ -36,7 +40,7 @@ const Contacts = () => {
 
 			<section id="contact__form" className="contact__form">
 				<div className="container ">
-					<div className="contact__wrapper">
+					<div className="contact-form__wrapper">
 						<h2>We're here to help you</h2>
 
 						<FormContact />
---
+++ src/pages/contacts/DirectionsMap.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+
+const DirectionsMap = () => {
+	useEffect(() => {
+		mapboxgl.accessToken = 'pk.eyJ1Ijoic29maWFvbGl2ZWlyYTU3IiwiYSI6ImNsdmlmcHA4YzFicmcya252b2kxbGxtb3cifQ.SClG-XkJ5FHKl3vU7RyaEQ';
+		const map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/sofiaoliveira57/clvwlfrh1028301qp0z926psj',
+			center: [-9.14841, 38.74316],
+			zoom: 13,
+		});
+
+		const directions = new MapboxDirections({
+			accessToken: mapboxgl.accessToken,
+			unit: 'metric',
+		});
+
+		map.addControl(directions, 'top-left');
+		
+
+		return () => map.remove();
+	}, []);
+	return <div id="map" style={{ width: '600px', height: '400px', borderRadius: '5px' }}></div>;
+};
+
+export default DirectionsMap;
