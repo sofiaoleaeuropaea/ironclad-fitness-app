3f26a6d4eb8d790baf0632fd88dfeb9a6c21a85c
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-05-17 13:13:52 +0100

    Correct bug on scroll reveal

--- src/components/ScrollReveal.jsx
+++ src/components/ScrollReveal.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 
 const ScrollReveal = ({ children }) => {
 	return (
-		<div style={{ position: 'relative', overflow: 'hidden' }}>
+		<div>
 			<motion.div variants={{ hidden: { opacity: 0, y: 75 }, visible: { opacity: 1, y: 0 } }} initial="hidden" animate="visible" transition={{ duration: 0.5 }}>
 				{children}
 			</motion.div>
